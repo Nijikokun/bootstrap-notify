ab62a9f86cdf86f3bbcd8ec3b24c814900063d8d
rubenstolk <ruben.stolk@changer.nl>
2013-02-01 12:45:04 +0530

    Update js/bootstrap-notify.js
    Without too many braces :)

--- js/bootstrap-notify.js
+++ js/bootstrap-notify.js
@@ -1,16 +1,17 @@
 /**
- * bootstrap-notify.js v1.0
+ * bootstrap-notify.js v1.0.0
  * --
-  * Copyright 2012 Goodybag, Inc.
+ * Copyright 2012 Nijiko Yonskai <nijikokun@gmail.com>
+ * Copyright 2012 Goodybag, Inc.
  * --
- * Licensed under the Apache License, Version 2.0 (the 'License');
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -21,47 +22,30 @@
     // Element collection
     this.$element = $(element);
     this.$note    = $('<div class="alert"></div>');
-    this.options  = $.extend(true, {}, $.fn.notify.defaults, options);
+    this.options  = $.extend(true, $.fn.notify.defaults, options, this.$element.data());
 
     // Setup from options
-    if(this.options.transition) {
-      if(this.options.transition === 'fade') {
+    if(this.options.transition)
+      if(this.options.transition == 'fade')
         this.$note.addClass('in').addClass(this.options.transition);
-      }
-      else {
-        this.$note.addClass(this.options.transition);
-      }
-    }
-    else {
-      this.$note.addClass('fade').addClass('in');
-    }
+      else this.$note.addClass(this.options.transition);
+    else this.$note.addClass('fade').addClass('in');
 
-    if(this.options.type) {
+    if(this.options.type)
       this.$note.addClass('alert-' + this.options.type);
-    }
-    else {
-      this.$note.addClass('alert-success');
-    }
+    else this.$note.addClass('alert-success');
 
-    if(!this.options.message && this.$element.data('message') !== '') {// dom text
-      this.$note.html(this.$element.data('message'));
-    }
-    else {
-      if(typeof this.options.message === 'object') {
-        if(this.options.message.html) {
-          this.$note.html(this.options.message.html);
-        }
-        else if(this.options.message.text) {
-          this.$note.text(this.options.message.text);
-        }
-      }
-      else {
+    if(this.options.message)
+      if(typeof this.options.message === 'string')
         this.$note.html(this.options.message);
-      }
-    }
+      else if(typeof this.options.message === 'object')
+        if(this.options.message.html)
+          this.$note.html(this.options.message.html);
+        else if(this.options.message.text)
+          this.$note.text(this.options.message.text);
 
     if(this.options.closable) {
-      var link = $('<a class="close pull-right" href="#">&times;</a>');
+      var link = $('<a class="close pull-right">&times;</a>');
       $(link).on('click', $.proxy(onClose, this));
       this.$note.prepend(link);
     }
@@ -104,5 +88,5 @@
     message: null,
     onClose: function () {},
     onClosed: function () {}
-  };
+  }
 })(window.jQuery);
